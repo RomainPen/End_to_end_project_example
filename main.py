8c9c408913c613540da92d034d79eba2824a391f
RomainPen <romain.pen.pro16@gmail.com>
2024-07-21 14:22:17 +0200

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import yaml
 from sklearn.model_selection import train_test_split
 import pickle
 from sklearn.metrics import f1_score
-
 from src.data_visualization.EDA import EDA
 from src.data_treatment.data_scaling import Scaling
 from src.model.model_building import Model_building
